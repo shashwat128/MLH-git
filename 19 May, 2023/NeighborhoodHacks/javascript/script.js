3de825b3475b8e33d27e409ed73b1b3721be0a89
Shashwat Singh <shashwat2048@gmail.com>
2023-05-21 19:38:14 +0530

    merge the next line of codes

--- 19 May, 2023/NeighborhoodHacks/javascript/script.js
+++ 19 May, 2023/NeighborhoodHacks/javascript/script.js
@@ -1,11 +1,43 @@
-let searchForm =document.querySelector('search-form');
+let searchForm = document.querySelector(".search-form");
+let shoppingCart = document.querySelector(".shopping-cart");
+let loginForm = document.querySelector(".login-form");
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#search-btn').onclick =() =>{
-    searchForm.classList.toggle('active');
-}
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
 
-let shoppingCart =document.querySelector('.shopping-cart');
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
-document.querySelector('#vart-btn').onclick =() =>{
-    shoppingCart.classList.toggle('active');
-}
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
