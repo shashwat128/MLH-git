d60852e4fb5d6000892a38bb3b9fc8fbc6f58f52
Shashwat Singh <shashwat2048@gmail.com>
2023-05-31 22:10:30 +0530

    review and blog

--- 19 May, 2023/NeighborhoodHacks/javascript/script.js
+++ 19 May, 2023/NeighborhoodHacks/javascript/script.js
@@ -62,3 +62,24 @@ var swiper = new Swiper(".product-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
